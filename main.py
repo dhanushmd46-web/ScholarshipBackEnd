529173395f938747c34d12a4748dd256b79b9ba0
dhanushmd46-web <dhanushmd46@gmail.com>
2026-06-18 15:00:30 +0530

    Your descriptive message here changed

--- main.py
+++ main.py
@@ -2,30 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from database import Base, engine
-from routers import auth, scholarship,application
-from models.user import UserTable 
+from routers import auth, scholarship, application
+from models.user import UserTable
 from models.scholarship import ScholarshipTable
 from models.application import ApplicationTable
- 
+
 load_dotenv()
- 
 app = FastAPI()
- 
 Base.metadata.create_all(bind=engine)
- 
+
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://scholarship-front-end.vercel.app",
+    "https://*.vercel.app",                       
 ]
- 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
- 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(scholarship.router, prefix="/scholarship", tags=["scholarship"])
 app.include_router(application.router, prefix="/application", tags=["application"])
